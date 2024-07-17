6eb52020b634551557b54881ce44dbec78793210
Sam Ebenezer N <samhonkd007@gmail.com>
2024-07-17 21:28:49 +0530

    Update Note.jsx

--- src/Note.jsx
+++ src/Note.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-
-const Note = ({ note, index, deleteNote }) => {
-  return (
-    <div className="note">
-      <span>{note.text}</span>
+import React from 'react'; 
+ 
+const Note = ({ note, index, deleteNote }) => { 
+  return ( 
+    <div className="note"> 
+      <span>{note.text}</span> 
       <button onClick={() => deleteNote(index)}>Delete</button> 
     </div>
   );
